7c04bc3ab52717818f29b1340439cfb3bcf9181e
VillageR88 <villager88pl@gmail.com>
2024-07-12 14:58:06 +0200

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def index():
+        siteTitle = "Blog preview card"
+        learning = "Learning"
+        title = "HTML & CSS foundations"
+        description = "These languages are the backbone of every website, defining structure, content, and presentation."
+        author = "Greg Hooper"
+        published_date = "Published 21 Dec 2023"
+        return render_template('index.html', siteTitle = siteTitle, learning=learning, title=title, description=description, author=author, published_date=published_date)
+
+    @app.route('/robots.txt')
+    def robots_txt():
+        return send_from_directory(app.static_folder, 'robots.txt')
+
+    return app
+
+
+    # if __name__ == '__main__':
+    #     app.run(debug=True)
